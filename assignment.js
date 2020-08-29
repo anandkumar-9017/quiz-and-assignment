a5ac720b555f8e2ecbdcfc0a2b3f163539222c43
LENOVO <anandkum9182@gmail.com>
2020-08-30 00:37:40 +0530

    first comit

---
+++ assignment.js
@@ -0,0 +1,21 @@
+var myNodelist=document.getElementsByTagName('LI');
+var i;
+for(i=0;i<myNodelist.length;i++)
+{
+	var span= document.createElement("SPAN");
+	var txt= document.createTextNode("\u00D7");
+	span.className="close";
+	span.appendChild(txt);
+	myNodelist[i].appendChild(span);
+}
+
+var close= document.getElementsByClassName(close);
+var i;
+for(i=0;i<close.length;i++)
+{
+	close[i].onclick= function(){
+		var div= this.parentelement;
+		div.style.display="none";
+	}
+
+}
